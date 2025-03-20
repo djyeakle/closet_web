5cc2104437e7aeedd657dcace926aada2f5095ab
Delaney <djyeakl@pointpark.edu>
2025-03-20 10:34:23 -0400

    fixed login auth

--- js/loginAuth.js
+++ js/loginAuth.js
@@ -16,13 +16,13 @@ document.addEventListener("DOMContentLoaded", function () {
             $.toaster({priority : 'danger', title : 'Error', message : 'Oops something broke'});
 
         } else if(data.accountID == 1 && data.username == "djyeakle" && data.password == "vbnmrno4") {
-            window.open(`https://closet-app.onrender.com/${data.username}Login.html`);
+            window.open(`https://closet-app.onrender.com/${data.username}login`);
 
         } else if(data.accountID == 11 && data.username == "aperenovich" && data.password == "bagel5") {
-            window.open(`https://closet-app.onrender.com/${data.username}Login.html`);
+            window.open(`https://closet-app.onrender.com/${data.username}login`);
 
         } else if(data.accountID == 28 && data.username == "amhalbleib" && data.password == "woanifbiau") {
-            window.open(`https://closet-app.onrender.com/${data.username}Login.html`);
+            window.open(`https://closet-app.onrender.com/${data.username}login`);
 
         } else {
             $.toaster({priority : 'danger', title : 'Error', message : 'Oops something broke'});
