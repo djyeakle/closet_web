56f090aec8f4bbac18c1aa6599f2ae40ad7c4fd2
Delaney <djyeakl@pointpark.edu>
2025-03-20 13:41:16 -0400

    added stuff for lain

---
+++ js/getLainClothes.js
@@ -0,0 +1,41 @@
+const API_URL = "https://closet-app.onrender.com/api/lainclothes";
+
+fetch(API_URL)
+.then(response => response.json())
+.then(data => {
+    const formattedData = data.map(closet => [
+        closet.itemid,
+        closet.type,
+        closet.color,
+        closet.size,
+        closet.season,
+        closet.rating
+    ]);
+
+    new gridjs.Grid({
+        columns: ["ItemID","Type","Color","Size","Season","Rating"],
+        data: formattedData,
+        search: true,
+        sort: true,
+        pagination: {
+            enabled: true,
+            limit: 5
+        },
+        resizable: true,
+        style: {
+            table: {
+                border: "1px solic #ccc"
+            },
+            th:{
+                "background-color": "#f4f4f4",
+                "text-align": "left"
+            },
+            td: {
+                "padding": "8px",
+                "border-bottom": "1px solid #ddd"
+            }
+        }
+    }).render(document.getElementById("grid-container"))
+    
+})
+.catch(error => console.error('Error fetching data', error))
---
+++ js/postLainClothes.js
@@ -0,0 +1,25 @@
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', event => {
+    event.preventDefault();
+    const formData = new FormData(formEl);
+    const data = Object.fromEntries(formData);
+
+    //validate that the fields have data results
+    if(data.itemid == "" || data.type == "" || data.color == "" || data.season == "" || data.rating == "") {
+        $.toaster({priority : 'danger', title : 'Error', message : 'Oops something broke'});
+    }
+    else {
+        fetch('https://closet-app.onrender.com/api/lainclothes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(res => res.json())
+          .then(data => console.log(data))
+          .then(error => console.log(error))
+          $.toaster({priority : 'success', title : 'Item Added', message : 'Awesome Find!'});
+    }
+
+});
